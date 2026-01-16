081cf1104f751d2226e61fafe9194b822eceadb0
Hong Nhung <hongnhung2001thd@gmail.com>
2026-01-17 02:38:03 +0900

    Fix deploy

--- manage.py
+++ manage.py
@@ -7,8 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    if os.environ.get('CREATE_SUPERUSER'):
-        import createsuperuser_render
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,3 +21,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
