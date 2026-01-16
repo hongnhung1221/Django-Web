2e260e5680dea9ad39096a0d4b791001613c3f6a
Hong Nhung <hongnhung2001thd@gmail.com>
2026-01-16 22:11:51 +0900

    Deploy

--- backend/settings.py
+++ backend/settings.py
@@ -109,10 +109,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
 
 
